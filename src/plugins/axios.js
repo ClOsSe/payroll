f0960e4368e4caf0786c14c85755d6ab2070dff2
Yashar@APNK_win <GrayGroup.co@gmail.com>
2020-09-29 18:15:52 +0330

    faz 1

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 var axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://arkaapi.nodedotjs.ir",
   headers: {
     Authorization: localStorage.token,
   },
 });
-export { axiosInstance }
+export { axiosInstance };
